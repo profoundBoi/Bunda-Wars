bf87ececb0495897dc5cec03786a6a46b59dd970
profoundBoi <connor170103@gmail.com>
2026-01-08 14:10:37 +0200

    first push

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed = 6f;
+    public float jumpHeight = 1.5f;
+    public float gravity = -9.81f;
+
+    private CharacterController controller;
+    private Vector2 moveInput;
+    private Vector3 velocity;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Move();
+        ApplyGravity();
+    }
+
+    void Move()
+    {
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+    }
+
+    void ApplyGravity()
+    {
+        if (controller.isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    // INPUT SYSTEM EVENTS
+    void OnMove(UnityEngine.InputSystem.InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void OnJump()
+    {
+        if (controller.isGrounded)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+    }
+}
